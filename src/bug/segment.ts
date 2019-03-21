651f077cf00f3368a2a7ded44390dbadab613e77
Adrian Lafond <mail@adrianlafond.com>
2019-03-20 22:00:34 -0400

    use angle.delta

--- src/bug/segment.ts
+++ src/bug/segment.ts
@@ -51,8 +51,9 @@ export default class Segment {
     const { target, vector, vectorStart, maxDistance, maxRotation } = this.model;
     const distance = Math.min(maxDistance, Point.distance(target, vectorStart.point));
 
-    let targetRadians = Math.atan2(target.y - vectorStart.y, target.x - vectorStart.x);
-    vector.radians += (targetRadians > vector.radians) ? 0.05 : -0.05;
+    const targetRadians = Math.atan2(target.y - vectorStart.y, target.x - vectorStart.x);
+    const deltaRadians = Angle.delta(vector.radians, targetRadians);
+    vector.radians += deltaRadians >= 0 ? 0.05 : -0.05;
 
     vector.x = vectorStart.x + Math.cos(vector.radians) * (distance * progress);
     vector.y = vectorStart.y + Math.sin(vector.radians) * (distance * progress);
